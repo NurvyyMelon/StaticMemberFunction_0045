24716198e8083ffbc48a1b0db07c10131a106b17
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-04 11:22:34 +0700

    Perbaiki format kelas angka dengan menambahkan spasi dan memperbaiki penempatan metode isiData

--- destructor.cpp
+++ destructor.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 using namespace std;
+
 class angka
 {
 private:
     int *arr;
     int panjang;
-    public:
+
+public:
     angka(int);      // Constructor
     ~angka();        // Destructor
     void cetakData();
     void isiData();
 };
+
 angka::angka(int i){ // Constructor
     panjang = i;
     arr = new int[i];
     isiData();
 }
+
 angka::~angka(){ // Destructor
     cout << endl;
     cetakData();
     delete[] arr;
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+
 void angka::cetakData()
 {
     for (int i = 1; i <= panjang; i++)
     {
         cout << i << " = " << arr[i] << endl;
     }
-    void angka::isiData()
+}
+
+void angka::isiData()
 {
     for (int i = 1; i <= panjang; i++)
     {
@@ -37,6 +44,7 @@ void angka::cetakData()
     }
     cout << endl;
 }
+
 int main()
 {
     angka belajarcpp(3);                    // Constructor Dipanggil
